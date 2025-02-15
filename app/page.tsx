b6021d2877dd0698fa688b5d5a19b01057f8b27e
Andrew Krier <ak513@duke.edu>
2025-02-15 12:01:01 -0500

    actually use mdx as a component

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import { openSans } from "../styles/fonts";
+import Intro from "@/app/intro.mdx";
 
 // Helpful for App Routing structure for Next.js:
 // https://nextjs.org/docs/app/getting-started/project-structure#colocation
@@ -24,22 +25,7 @@ export default function Home() {
                 style={{ borderRadius: "16px" }}
             />
             <div>
-                <h1>Hello!</h1>
-                <p>
-                    my name is Andrew Krier, i’m from Minnesota, and i love
-                    making music, playing sports, and spending time with
-                    friends.
-                </p>
-                <p>
-                    i’m currently a software engineer for Duolingo in
-                    Pittsburgh, Pennsylvania.
-                </p>
-                <p>other hobbies include:</p>
-                <ul style={{ listStyleType: "-moz-initial" }}>
-                    <li>too many fantasy football leagues</li>
-                    <li>relying on legos for interior decoration</li>
-                    <li>ranked foosball</li>
-                </ul>
+                <Intro />
             </div>
         </div>
     );
